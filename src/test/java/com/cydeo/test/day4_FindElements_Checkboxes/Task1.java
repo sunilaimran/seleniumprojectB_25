eafc8c0fb236666d0ac2217ce5d9d563146eef6c
sunila <imransunila@yahoo.com>
2022-02-16 12:14:16 -0500

    day4_task1

--- src/test/java/com/cydeo/test/day4_FindElements_Checkboxes/Task1.java
+++ src/test/java/com/cydeo/test/day4_FindElements_Checkboxes/Task1.java
@@ -35,12 +35,34 @@ public class Task1 {
         WebElement password_ex2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
 
         // c. “E-mail” text
-        WebElement emailLabel = driver.findElement(By.xpath(""));
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
 
-       // d. E-mail input box
-       // e. “Retrieve password” button
-       // f. “Powered by Cydeo text
-       // 4. Verify all web elements are displayed.
+        //d. E-mail input box
+        WebElement inputBox_ex1 = driver.findElement(By.xpath("//input[@name='email']"));
+
+        //Locate inputBox using xpath contains method
+
+        //tagName[contains(@attribute,'value')]
+
+        WebElement inputBox_ex2 = driver.findElement(By.xpath("//input[contains(@pattern,'a-z')]"));
+
+
+        // e. “Retrieve password” button
+        //button[@type='submit']
+        //button[@class='radius']
+        WebElement retrievePasswordBtn = driver.findElement(By.xpath("//button[@id='form_submit']"));
+
+        // f. “Powered by Cydeo text
+        WebElement CydeoText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
+        // 4. Verify all web elements are displayed.
+        System.out.println("HomeLink_ex1.isDisplayed() = " + HomeLink_ex1.isDisplayed());
+        System.out.println("password_ex1.isDisplayed() = " + password_ex1.isDisplayed());
+        System.out.println("emailLabel.isDisplayed() = " + emailLabel.isDisplayed());
+        System.out.println("inputBox_ex1.isDisplayed() = " + inputBox_ex1.isDisplayed());
+        System.out.println("retrievePasswordBtn.isDisplayed() = " + retrievePasswordBtn.isDisplayed());
+        System.out.println("CydeoText.isDisplayed() = " + CydeoText.isDisplayed());
+
         // First solve the task with using cssSelector only. Try to create 2 different
         // cssSelector if possible
 
